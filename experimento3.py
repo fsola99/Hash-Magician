76420ae25b3dd8370b08c9439f0cdfc8f23e22b5
Dzukito <30675879+Dzukito@users.noreply.github.com>
2023-04-04 16:45:59 -0300

    Agrego control de que pasa si no elige carpeta

---
+++ experimento3.py
@@ -0,0 +1,89 @@
+import argparse
+import hashlib
+import os
+import csv
+import PySimpleGUI as sg
+
+# Definir el diseño de la interfaz gráfica
+layout = [
+    [sg.Text('Seleccionar carpeta objetivo:'), sg.Input(key='-FOLDER-', readonly=True), sg.FolderBrowse()],
+    [sg.Text('Seleccionar algoritmos hash a utilizar:')],
+    [sg.Checkbox('MD5', default=True, key='-MD5-'), sg.Checkbox('SHA1', default=True, key='-SHA1-'), sg.Checkbox('SHA256', default=True, key='-SHA256-')],
+    [sg.Checkbox('Mayúsculas', default=False, key='-UPPER-')],
+    [sg.Button('Iniciar'), sg.Button('Cancelar')]
+]
+
+# Crear la ventana
+sg.set_global_icon('./mago.ico')
+window = sg.Window('Hash-Magician', layout)
+
+# Loop principal para eventos de la interfaz gráfica
+while True:
+    event, values = window.read()
+    
+    # Si el usuario cierra la ventana o hace clic en 'Cancelar'
+    if event == sg.WINDOW_CLOSED or event == 'Cancelar':
+        break
+    
+    # Si el usuario hace clic en 'Iniciar', ademas corrobora que haya elegido una carpeta
+    if event == 'Iniciar':
+        folder_path = values['-FOLDER-']
+        if folder_path=="" :
+          sg.popup('No se ha seleccionado una carpeta. Por favor, seleccione una carpeta y vuelva a intentarlo.',title="Error")
+          continue
+  
+        
+        # Seleccionar los algoritmos hash a utilizar
+        algoritmos_seleccionados = []
+        if values['-MD5-']:
+            algoritmos_seleccionados.append('md5')
+        if values['-SHA1-']:
+            algoritmos_seleccionados.append('sha1')
+        if values['-SHA256-']:
+            algoritmos_seleccionados.append('sha256')
+        
+        # Configurar los argumentos
+        parser = argparse.ArgumentParser(description='Obtener el hash de todos los archivos en una carpeta')
+        parser.add_argument('-p', '--path', type=str, default=folder_path, help='Carpeta objetivo (por defecto: carpeta seleccionada)')
+        parser.add_argument('-a', '--algoritmo', nargs='+', default=algoritmos_seleccionados, choices=['sha1', 'sha256', 'md5'], help='Algoritmo(s) hash a utilizar (por defecto: seleccionados)')
+        args = parser.parse_args()
+
+        # Calcular los hashes de los archivos
+        hash_list = []
+        def calcular_hashes(folder_path, file_list, hash_list):
+            for file_name in file_list:
+                file_path = os.path.join(folder_path, file_name)
+
+                if os.path.isfile(file_path):
+                    with open(file_path, 'rb') as file_current:
+                        content = file_current.read()
+           
+                        md5_value = hashlib.md5(content).hexdigest()
+                        sha1_value = hashlib.sha1(content).hexdigest()
+                        sha256_value = hashlib.sha256(content).hexdigest()
+                        if values['-UPPER-']:
+                            md5_value = md5_value.upper()
+                            sha1_value = sha1_value.upper()
+                            sha256_value = sha256_value.upper()
+                        hash_list.append({'filename': file_name, 'md5':md5_value, 'sha256':sha256_value, 'sha1': sha1_value})
+                else:
+                    calcular_hashes(file_path, os.listdir(file_path), hash_list)
+            return hash_list
+
+        hashes = calcular_hashes(folder_path, os.listdir(folder_path), hash_list)
+        
+        #Output
+        output_path = os.path.join(folder_path, 'hashes.csv')
+        with open(output_path, 'w', newline='') as csvfile:
+            fieldnames = ['filename', 'md5', 'sha1', 'sha256']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for hash_dict in hashes:
+                writer.writerow(hash_dict)
+                
+        sg.popup(f'Se ha creado el archivo CSV en: {output_path}',title="Ejecucion completada")
+        
+        
+
+    
+window.close()
